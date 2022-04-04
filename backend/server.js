ce6bbedc82658ba750e93df7cccc9f900d830c95
keyur diwan <keyurdiwan009@gmail.com>
2022-04-04 09:56:01 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 const app = require( './app' );
 const { connectDatabase } = require( './config/database' );
-
-
+const cloudinary = require("cloudinary");
 connectDatabase();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 app.listen( process.env.PORT, () => {
 
 
-    console.log(`server is running..! ${process.env.PORT}`);
-})
+    console.log( `server is running..! ${ process.env.PORT }` );
+} );
